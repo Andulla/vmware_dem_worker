0a0ec6ccbcbbac3c98fd30fb0b09d6bdc0098c1c
Andulla <andreas.wilke@puppetlabs.com>
2015-10-27 21:37:19 +0100

    Next test

--- lib/facter/vmwaredemworker.rb
+++ lib/facter/vmwaredemworker.rb
@@ -1,6 +1,6 @@
 Facter.add(:vmwaredemworker) do
   confine :kernel => 'windows'
   setcode do
-    Facter::Core::Execution.exec("sc queryex type= service state= all")
+    Facter::Core::Execution.exec('sc queryex type= service state= all | find /i "VMware DEM-Worker"')
   end
 end
