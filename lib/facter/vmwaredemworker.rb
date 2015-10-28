d3f9b944693c55bf452047d85cca0a4beac45635
Andulla <andreas.wilke@puppetlabs.com>
2015-10-28 10:25:47 +0100

    Remove old facter

--- lib/facter/vmwaredemworker.rb
+++
@@ -1,6 +0,0 @@
-Facter.add(:vmwaredemworker) do
-  confine :kernel => 'windows'
-  setcode do
-    Facter::Core::Execution.exec('sc queryex type= service state= all | find /i "VMware DEM-Worker"')
-  end
-end
